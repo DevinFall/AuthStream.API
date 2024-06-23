70eb20c19aaf792ab4c1fc61d4043d5c713ef8ab
Devin Fall <devin.fall@outlook.com>
2024-06-23 10:05:44 -0700

    Added environment variables to builder configuration.

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Configuration.AddEnvironmentVariables();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.

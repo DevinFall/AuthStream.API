19dea37d394580f99ca2d322f4df306ead1d7409
Devin Fall <devin.fall@outlook.com>
2024-06-23 10:10:57 -0700

    Added ApplicationDbContext.cs.

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,6 +1,14 @@
+using AuthStream.API.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace AuthStream.API.Data;
 
 public class ApplicationDbContext : DbContext
 {
-    
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<User> Users => Set<User>();
 }

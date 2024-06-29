0854ae25e0be3f9c1e01a6e89bed16beefbcd5a4
Devin Fall <devin.fall@outlook.com>
2024-06-29 13:55:44 -0700

    Added DTOs.

---
+++ DTOs/RegisterForm.cs
@@ -0,0 +1,8 @@
+namespace AuthStream.API.DTOs;
+
+public class RegisterForm
+{
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
